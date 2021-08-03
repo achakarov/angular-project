b31d9ae835dce3f7d1bcc6f1f5406910cba394f4
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-08-03 22:50:52 +0300

    Installed Angular Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCnFvqnuJPqXeK2ihjw487hpb_qK5sLd8Q',
+    authDomain: 'my-blog-e2139.firebaseapp.com',
+    databaseURL: 'https://my-blog-e2139.firebaseio.com',
+    projectId: 'my-blog-e2139',
+    storageBucket: 'my-blog-e2139.appspot.com',
+    messagingSenderId: '856055597930',
+    appId: '1:856055597930:web:1ba70da7af19f34cf3f313',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCnFvqnuJPqXeK2ihjw487hpb_qK5sLd8Q',
+    authDomain: 'my-blog-e2139.firebaseapp.com',
+    databaseURL: 'https://my-blog-e2139.firebaseio.com',
+    projectId: 'my-blog-e2139',
+    storageBucket: 'my-blog-e2139.appspot.com',
+    messagingSenderId: '856055597930',
+    appId: '1:856055597930:web:1ba70da7af19f34cf3f313',
+  },
 };
 
 /*
