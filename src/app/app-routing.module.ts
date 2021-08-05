b22f90ca36bcee0f5aee1b32079374cfc59fad8d
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-08-05 20:57:27 +0300

    added routes for details and edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsPostComponent } from './components/details-post/details-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -17,6 +19,14 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'edit',
+    component: EditPostComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsPostComponent,
   },
 ];
 
