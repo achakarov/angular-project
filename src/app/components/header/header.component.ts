7512b0b116b8b83a01c240ea53aa57010f45db82
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-08-09 22:50:36 +0300

    showing different view for logged in and guest

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,24 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(public firebaseAuth: AngularFireAuth) {}
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get email(): string {
+    return this.userService.user?.email || '';
+  }
+  constructor(
+    public firebaseAuth: AngularFireAuth,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   handleLogout() {
     this.firebaseAuth.signOut();
     console.log('Logged out!');
+    this.router.navigate(['/']);
   }
 }
